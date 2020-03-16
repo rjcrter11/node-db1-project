e4e1129d48ddfe7e8258b3268e7d06f90c136f84
rjcrter11 <rjcrter11@gmail.com>
2020-03-16 16:30:13 -0500

    finished the stretch goals

--- api/server.js
+++ api/server.js
@@ -10,12 +10,28 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.status(200).send("<h3>Node DB Project</h3>");
 });
+// ---- End Test ---- //
+
+// ---- Stretch get request ---- //
+
+server.get("/api/accounts", (req, res) => {
+  db("accounts")
+    .orderBy("id", "asc")
+    .limit(10)
+    .then((rows) => {
+      res.status(200).json(rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Ya did it wrong frand" });
+    });
+});
 
 server.get("/api/accounts", (req, res) => {
   db.select("*")
     .from("accounts")
     .then((rows) => {
-      res.status(200).json({ data: rows });
+      res.status(200).json(rows);
     })
     .catch((err) => {
       console.log(err);
@@ -28,11 +44,9 @@ server.get("/api/accounts/:id", (req, res) => {
     .where({ id: req.params.id })
     .first()
     .then((account) => {
-      if (account) {
-        res.status(200).json({ data: account });
-      } else {
-        res.status(404).json({ message: "Account not found" });
-      }
+      account
+        ? res.status(200).json({ data: account })
+        : res.status(404).json({ message: "Account not found" });
     })
     .catch((err) => {
       console.log(err);
@@ -64,11 +78,9 @@ server.put("/api/accounts/:id", (req, res) => {
     .where({ id: req.params.id })
     .update(changes)
     .then((count) => {
-      if (count > 0) {
-        res.status(200).json({ success: ` ${count} account updated` });
-      } else {
-        res.status(404).json({ message: "Account not found" });
-      }
+      count > 0
+        ? res.status(200).json({ success: `${count} account(s) updated` })
+        : res.status(404).json({ message: "Account not found" });
     })
     .catch((err) => {
       console.log(err);
@@ -81,11 +93,9 @@ server.delete("/api/accounts/:id", (req, res) => {
     .where({ id: req.params.id })
     .del()
     .then((count) => {
-      if (count > 0) {
-        res.status(200).json({ success: `${count} account(s) deleted` });
-      } else {
-        res.status(404).json({ message: "Account not found" });
-      }
+      count > 0
+        ? res.status(200).json({ success: `${count} account(s) deleted` })
+        : res.status(404).json({ message: "Account not found" });
     })
     .catch((err) => {
       console.log(err);
